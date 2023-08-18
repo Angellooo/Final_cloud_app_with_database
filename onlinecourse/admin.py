63f253a4b8d8be4974bce57b67e643346b706be0
Angello <angello.gonzalez03@.com>
2023-08-17 21:33:05 -0400

    Adding new models and Requirements update
    Adding new models Question, Choice and Submission to models.py. Also adding these models to admin.py with NestedTabularInline from django_nested_admin package

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,24 +1,33 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
+from nested_admin import NestedModelAdmin, NestedTabularInline
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class ChoiceInLine(NestedTabularInline):
+    model = Choice
+    extra = 2
 
+class QuestionInLine(NestedTabularInline):
+    inlines = [ChoiceInLine]
+    model = Question
+    extra = 1
 
-class LessonInline(admin.StackedInline):
+class LessonInline(NestedTabularInline):
+    inlines = [QuestionInLine]
     model = Lesson
-    extra = 5
+    extra = 0
 
 
 # Register your models here.
-class CourseAdmin(admin.ModelAdmin):
+class CourseAdmin(NestedModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
 
-class LessonAdmin(admin.ModelAdmin):
+class LessonAdmin(NestedModelAdmin):
     list_display = ['title']
 
 
@@ -28,3 +37,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question)
+admin.site.register(Choice)
